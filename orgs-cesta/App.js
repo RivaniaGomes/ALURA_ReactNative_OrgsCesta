da77b4df619018858b6df0f67ae015c56ae3ca3d
RivaniaGomes <rivaniagomes@gmail.com>
2022-11-15 17:52:02 -0300

    Componentização do texto.

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
+import React from "react";
+import { StatusBar, SafeAreaView, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 
-import Cesta from './src/telas/Cesta.jsx';
+import Cesta from "./src/telas/Cesta.jsx";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    MontserratRehular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <View />;
+  }
+
   return (
     <SafeAreaView>
-      <StatusBar/>
+      <StatusBar />
       <Cesta />
     </SafeAreaView>
   );
 }
-
